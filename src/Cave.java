4589bcaf760c3d1059a73114b30606985f3a3de5
jordy muniz <jordy.muniz96@gmail.com>
2021-01-18 10:31:25 -0600

    intro

---
+++ src/Cave.java
@@ -0,0 +1,24 @@
+import  java.util.Scanner;
+
+
+public class Cave {
+
+    public static void game(){
+        Scanner decision = new Scanner(System.in);
+        System.out.println("BOOM!!!");
+        System.out.println(".......");
+        System.out.println("It looks like the airplane you were riding has suddenly crashed into a cave...");
+        System.out.println("you will need to fight your way out using your weapons");
+        System.out.println("It appears you only have a axe, sticks, a knife, and a lighter");
+        System.out.println("there's a goblin up ahead blocking you path, what would you like to attack it with?");
+        System.out.println(1 + ": axe");
+        System.out.println(2 + ": Stick");
+        System.out.println(3 + ": knife");
+        System.out.println(4 + ": lighter");
+        int choice = decision.nextInt()
+        if (choice == 1) {
+
+
+        }
+    }
+}
