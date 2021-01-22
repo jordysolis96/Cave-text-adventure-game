2f45cefabd14eeb02263f2be1b9bd5318f63dddf
jordy muniz <jordy.muniz96@gmail.com>
2021-01-21 22:01:03 -0600

    King boss fight

--- src/FireKingDemon.java
+++ src/FireKingDemon.java
@@ -18,51 +18,62 @@ public class FireKingDemon {
         System.out.println(8 + ": Throw explosives");
         System.out.println(9 + ": Use sword");
         int Choice = choice.nextInt();
-        if(choice == 1){
-            System.out.println("You chop the serpents head off! A strange person comes out of the mouth");
-            System.out.println("He lunges forward with nothing but his hands");
-            System.out.println("What will you do?");
-            System.out.println(1 + ": Fist fight");
-            System.out.println(1 + ": Use the axe again");
-            int choice2 = Choice.nextInt();
-            if(choice2 == 1){
-                System.out.println("You beat him to death");
-                System.out.println("You collected a pouch of explosives");
-            }else if(choice2 == 2){
-                System.out.println("He grabs the axe mid swing and kills you!!");
-                System.out.println("Game over!");
-                respawn();
-            }
-        }else if(choice == 2){
-            System.out.println("The serpent eats you alive");
+        if(Choice == 1){
+            System.out.println("The King blocks and stabs you.");
+            System.out.println("Game Over!");
+            respawn();
+        }else if(Choice == 2){
+            System.out.println("A stick won't work, you got slapped to death!");
             System.out.println("Game over!");
             respawn();
-        }else if(choice == 3){
-            System.out.println("The knife does nothing to the large serpent...");
-            System.out.println("It smacks you with it's tail and you splatter on the wall to your death!");
+        }else if(Choice == 3){
+            System.out.println("The knife won't penetrate the kings rough skin, you get stomped to death.");
             System.out.println("Game over!");
             respawn();
-        }else if(choice == 4){
-            System.out.println("It swerves to the left and eats you!");
+        }else if(Choice == 4){
+            System.out.println("You get slammed to death!");
             System.out.println("Game over");
             respawn();
-        }else if(choice == 5){
-            System.out.println("The Serpent crushes you to death");
+        }else if(Choice == 5){
+            System.out.println("The king crushes you to death");
             System.out.println("Game Over");
             respawn();
-        }else if(choice == 6){
-            System.out.println("You use the staff and to your luck it burns the Serpent!");
-            System.out.println("You collected a pouch of explosives.");
-        }else if(choice == 7){
-            System.out.println("The rope won't work...");
-            Serpent();
-        }else if(choice == 8){
-            System.out.println("You can't out run a Serpent..");
-            Serpent();
+        }else if(Choice == 6){
+            System.out.println("You get crushed to death!");
+            System.out.println("Game over!");
+            respawn();
+        }else if(Choice == 7){
+            System.out.println("You weave the next attack and choke the demon to death with the rope");
+            System.out.println("Congrats on beating the demon king!");
+            System.out.println("It looks like your rope burned in the process...");
+        }else if(Choice == 8){
+            System.out.println("That won't work, you get mauled to death");
+            respawn();
+        }else if(Choice == 9){
+            System.out.println("The sword doesn't work, you get stomped to death");
+            respawn();
         }else{
             System.out.println("invalid input");
-            Serpent();
+            FireDemon();
         }
     }
+    public static void respawn() {
+        Scanner choice = new Scanner(System.in);
+        System.out.println("would you like to retry?");
+        System.out.println(1 + ": last check point");
+        System.out.println(2 + ": restart game");
+        System.out.println(3 + ": exit");
+        int respawn = choice.nextInt();
+        if (respawn == 1) {
+            FireDemon();
+        } else if (respawn == 2) {
+            Cave.game();
+        } else if (respawn == 3) {
+            System.out.println("See Ya!");
+            System.exit(0);
+        } else {
+            System.out.println("invalid input");
+            respawn();
+        }
     }
 }
