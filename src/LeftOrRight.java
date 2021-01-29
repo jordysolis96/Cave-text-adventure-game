34423b20452a2079f687e9b26d09518e4b7aaea6
jordy muniz <jordy.muniz96@gmail.com>
2021-01-28 22:28:10 -0600

    Left or right option add

---
+++ src/LeftOrRight.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class LeftOrRight {
+    public static void leftOrRight(){
+        Scanner Choice = new Scanner(System.in);
+        System.out.println("What will you do?");
+        System.out.println(1 + ": go left");
+        System.out.println(2 + ": go right");
+        int choice = Choice.nextInt();
+        if(choice == 1){
+            System.out.println("The elevator moves to the left...");
+            System.out.println("It comes to a sudden stop. You hear something snap...");
+            System.out.println("You fall to your death!");
+            System.out.println("Game Over!");
+        }else if(choice == 2){
+
+        }
+    }
+}
