0c9721feee4f7b26c6f98ccbbabf44835b5838c6
jordy muniz <jordy.muniz96@gmail.com>
2021-01-29 12:14:04 -0600

    finished join choice

--- src/join.java
+++ src/join.java
@@ -7,5 +7,14 @@ public class join {
         System.out.println(1 + ": Join");
         System.out.println(2 + ": Deny offer");
         int choice = Choice.nextInt();
+        if (choice == 1) {
+            System.out.println("The twin princes: Excellent choice, this way.. if you will.");
+            System.out.println("The old king: I'm glad you made the right choice you will now be appointed as my royal guards!");
+            System.out.println("Congratulations! You have reached ending 3!");
+
+        }else if(choice == 2){
+            System.out.println("The twin princes:A poor choice you have made...");
+        }
     }
+
 }
