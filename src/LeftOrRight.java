7901d4c521e48eb9bcb6284b4120befb14fd3f67
jordy muniz <jordy.muniz96@gmail.com>
2021-01-28 22:37:17 -0600

    respawns added to left or right

--- src/LeftOrRight.java
+++ src/LeftOrRight.java
@@ -12,10 +12,30 @@ public class LeftOrRight {
             System.out.println("It comes to a sudden stop. You hear something snap...");
             System.out.println("You fall to your death!");
             System.out.println("Game Over!");
+            respawn();
         }else if(choice == 2){
             System.out.println("You move to the right.");
             System.out.println("Eventually you come to a stop.");
             System.out.println("You exit the elevator and you walk into an empty room...");
+        }
+    }
+    public static void respawn() {
+        Scanner choice = new Scanner(System.in);
+        System.out.println("would you like to retry?");
+        System.out.println(1 + ": last check point");
+        System.out.println(2 + ": restart game");
+        System.out.println(3 + ": exit");
+        int respawn = choice.nextInt();
+        if (respawn == 1) {
+            leftOrRight();
+        } else if (respawn == 2) {
+            Cave.game();
+        } else if (respawn == 3) {
+            System.out.println("See Ya!");
+            System.exit(0);
+        } else {
+            System.out.println("invalid input");
+            respawn();
         }
     }
 }
